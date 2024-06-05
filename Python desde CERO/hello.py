46ff204878d7b7a3bc54f97d587b0152d0c15df2
Matias Benavides <matias.benavides.rojas@gmail.com>
2024-06-05 16:50:48 -0400

    Tipos de datos type() y Comentarios

--- Python desde CERO/hello.py
+++ Python desde CERO/hello.py
@@ -1,2 +1,31 @@
-# Hola mundo
-print ("Hola Python")
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en Python
+print ("Hola Python")   # Cadena de Texto
+print('Hola Python')
+
+
+
+
+""""
+Este es un
+comentario
+en varias líneas
+"""
+'''
+Esto tambien es un
+comentario 
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str"))    # Tipo 'str'     <class 'str'>
+print(type(23))                   # Tipo 'int'     <class 'int'>
+print(type(2.3))                  # Tipo 'float'   <class 'float'>
+print(type(True))                 # Tipo 'bool'    <class 'bool'>
+print(type(1 + 3j))               # Tipo 'complex' <class 'complex'>
+print(type([1,2,3]))              # Tipo 'list'    <class 'list'>
+print(type({'nombre': 'Matiti'})) # Tipo 'dict'    <class 'dict'>
+print(type({9.8, 3.14, 2.4}))     # Tipo 'set'     <class 'set'>
+print(type((9.8, 3.14, 2.4)))     # Tipo 'tuple'   <class 'tuple'>
+
+# Es para asegurarse que un tipo de dato pertenezca a algo
