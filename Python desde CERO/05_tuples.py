79996bd92b26655ac006c33e5de47d74a2919250
Matias Benavides <matias.benavides.rojas@gmail.com>
2024-07-21 19:20:53 -0400

    Ejercicios con tuplas

--- Python desde CERO/05_tuples.py
+++ Python desde CERO/05_tuples.py
@@ -55,6 +55,97 @@ print(type(my_tuple)) # <class 'tuple'>
 
 #---------------------------------------------------------------------------------------#
 
+# Ejercicios #
+
+# Crea una tupla vacia
+latupla = ()
+
+# Crea una tupla que contenga los nombres de tus hermanas y tus hermanos (los hermanos imaginarios están bien)
+
+latupla = ("Mackarena", "Juan Pablo", "Mario")
+
+# Únase a tuplas de hermanos y hermanas y asígnelas a hermanos
+
+brothers = ("Juan Pablo", "Mario")
+
+sisters = ("Mackarena",) 
+
+siblings = brothers + sisters
+print(siblings) # ('Juan Pablo', 'Mario', 'Mackarena')
+
+"""""
+# Esto es una cadena
+sisters = ("Mackarena")  # Tipo: str
+
+# Esto es una tupla con un solo elemento
+sisters = ("Mackarena",)  # Tipo: tuple
+"""""
+# ¿Cuántos hermanos tiene usted?
+
+print(len(siblings)) # 3
+
+# Modifica la tupla de hermanos y agrega el nombre de tu padre y tu madre y asígnalo a family_members
+
+family_members = list(siblings)
+print(type(family_members))
+
+#family_members.insert(0, "Juan")
+#family_members.insert(1, "Adriana")
+
+padres = ["Juan", "Adriana"]
+family_members.extend(padres)
+
+print(family_members) #['Juan Pablo', 'Mario', 'Mackarena', 'Juan', 'Adriana']
+
+# Desempaquetar hermanos y padres de family_members
+
+(joven, adulto, pequeña, viejo, abuela) = family_members
+print(joven) # Juan Pablo
+print(adulto) # Mario
+print(pequeña) # Mackarena
+print(viejo) # Juan
+print(abuela) # Adriana
+
+# Crea tuplas de frutas, verduras y productos animales. Une las tres tuplas y asígnalas a una variable llamada food_stuff_tp.
+
+frutas = ("manzana", "banana", "pera", "sandia")
+verduras = ("tomate", "zanahoria", "papa", "cebolla")
+producto_animal = ("t-bone","guachalomo", "panita", "tuto de pollo")
+
+food_stuff_tp = frutas + verduras + producto_animal
+print(food_stuff_tp) # ('manzana', 'banana', 'pera', 'sandia', 'tomate', 'zanahoria', 'papa', 'cebolla', 't-bone', 'guachalomo', 'panita', 'tuto de pollo')
+
+# Cambie la tupla about food_stuff_tp a una lista food_stuff_lt
+food_stuff_tl = list(food_stuff_tp)
+print(type(food_stuff_tl)) # <class 'list'>
 
 
+# Corte el elemento o elementos del medio de la tupla food_stuff_tp o de la lista food_stuff_lt.
+
+print(food_stuff_tp[5:12]) #['zanahoria', 'papa', 'cebolla', 't-bone', 'guachalomo', 'panita', 'tuto de pollo']
+
+#Corte los primeros tres elementos y los últimos tres elementos de la lista food_staff_lt
+
+print(food_stuff_tl[3:12], food_stuff_tl[-3:12])
+# ['sandia', 'tomate', 'zanahoria', 'papa', 'cebolla', 't-bone', 'guachalomo', 'panita', 'tuto de pollo']
+# ['guachalomo', 'panita', 'tuto de pollo']
+
+# Eliminar completamente la tupla food_staff_tp
+del food_stuff_tp
+#print(food_stuff_tp) # NameError: name 'food_stuff_tp' is not defined.
+
+# Compruebe si existe un elemento en tupla:
+#Check if 'Estonia' is a nordic country
+#Check if 'Iceland' is a nordic country
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+if 'Estonia' in nordic_countries:
+    print("Estonia es un pais nordico")
+else:
+    print("Estonia no es un pais nordico")
+
+if 'Iceland' in nordic_countries:
+    print("Iceland es un pais nordico")
+else:
+    print("Iceland no es un pais nordico")
 
